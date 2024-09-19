99cdc446eb6aa3edb5948ea7e7797720715f7af6
octopus-idil <oncuidil@gmail.com>
2024-09-19 15:43:59 +0300

    Added navigation and passing objects

--- app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
+++ app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myrecipeapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -23,7 +24,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun RecipeScreen(modifier: Modifier=Modifier){
+fun RecipeScreen(modifier: Modifier=Modifier,
+                 navigateToDetail: (Category) -> Unit
+                 ){
     val recipeViewModel:MainViewModel= viewModel()
     val viewState by recipeViewModel.categoriesState
     
@@ -36,28 +39,35 @@ fun RecipeScreen(modifier: Modifier=Modifier){
                 Text("Error")
             }
             else->{
-                CategoryScreen(categories = viewState.list)
+                CategoryScreen(categories = viewState.list
+                    , navigateToDetail)
 
             }
         }
     }
 }
+
 @Composable
-fun CategoryScreen(categories: List<Category>){
+fun CategoryScreen(
+    categories: List<Category>,
+    navigateToDetail: (Category) -> Unit
+) {
     LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
-        items(categories) {
-            category ->
-            CategoryItem(category = category)
+        items(categories) { category ->
+            CategoryItem(category = category, navigateToDetail)
         }
     }
 }
 
 @Composable
-fun CategoryItem(category: Category) {
+fun CategoryItem(category: Category,
+                 navigateToDetail:(Category)->Unit
+                 ) {
     Column(
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxSize(),
+            .fillMaxSize()
+            .clickable { navigateToDetail(category) },
         horizontalAlignment = Alignment.CenterHorizontally
     )
     {
